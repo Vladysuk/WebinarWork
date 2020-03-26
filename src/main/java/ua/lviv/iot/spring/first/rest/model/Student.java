fba1c3821ce6844da102dd80033f358fe8b8a86a
Vlad Dorofeiev <vladik1543u7@gmail.com>
2020-03-26 10:17:11 +0200

    added new files for databases and updated some files

--- src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
@@ -1,10 +1,22 @@
 package ua.lviv.iot.spring.first.rest.model;
 
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
     private String firstName;
     private String lastName;
-    private int id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    public Student(){}
     public Student(String firstName, String lastName, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
